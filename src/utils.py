88becddd96e55b5e151d6ddf4eb27f764f9faf9a
Jingyuan <shijy2000@gmail.com>
2025-02-23 12:33:50 -0500

    Remove trainer class and loss tracking functionality from utils

--- src/utils.py
+++ src/utils.py
@@ -14,21 +14,3 @@ def set_seed(seed=42):
     os.environ['PYTHONHASHSEED'] = str(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
-
-class LossTracker:
-    def __init__(self):
-        self.history = defaultdict(list)
-        
-    def update(self, metrics):
-        for k, v in metrics.items():
-            self.history[k].append(v)
-            
-    def plot(self):
-        plt.figure(figsize=(10, 6))
-        for metric, values in self.history.items():
-            plt.plot(values, label=metric)
-        plt.xlabel('Epoch')
-        plt.ylabel('Loss')
-        plt.legend()
-        plt.grid(True)
-        plt.show()
